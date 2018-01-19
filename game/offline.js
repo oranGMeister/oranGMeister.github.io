7a5085490a5ecf36a5a6519678a705d0fcb11256
orangee <aff.ramos@campus.fct.unl.pt>
2018-01-19 13:19:17 +0000

    ~game

---
+++ game/offline.js
@@ -0,0 +1,27 @@
+﻿{
+	"version": 1516367936,
+	"fileList": [
+		"data.js",
+		"c2runtime.js",
+		"jquery-2.1.1.min.js",
+		"offlineClient.js",
+		"images/background-sheet0.png",
+		"images/alien-sheet0.png",
+		"images/player-sheet0.png",
+		"images/foreground-sheet0.png",
+		"images/playerbullet-sheet0.png",
+		"images/layoutlimit-sheet0.png",
+		"images/alienbullet-sheet0.png",
+		"images/particles.png",
+		"media/player shooting.m4a",
+		"media/player shooting.ogg",
+		"media/alien hit.m4a",
+		"media/alien hit.ogg",
+		"icon-16.png",
+		"icon-32.png",
+		"icon-114.png",
+		"icon-128.png",
+		"icon-256.png",
+		"loading-logo.png"
+	]
+}

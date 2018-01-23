6194e14ea8200c185217763da7343a09c69a5f8d
orangee <aff.ramos@campus.fct.unl.pt>
2018-01-23 18:05:59 +0000

    game

---
+++ game/offline.js
@@ -0,0 +1,26 @@
+﻿{
+	"version": 1516730730,
+	"fileList": [
+		"data.js",
+		"c2runtime.js",
+		"jquery-2.1.1.min.js",
+		"offlineClient.js",
+		"images/background-sheet0.png",
+		"images/plataform-sheet0.png",
+		"images/mario-sheet0.png",
+		"images/gamelimit-sheet0.png",
+		"images/barrel-sheet0.png",
+		"images/donkeykong-sheet0.png",
+		"images/stairsup-sheet0.png",
+		"images/stairsdown-sheet0.png",
+		"images/stairs-sheet0.png",
+		"images/princess-sheet0.png",
+		"images/hammer-sheet0.png",
+		"icon-16.png",
+		"icon-32.png",
+		"icon-114.png",
+		"icon-128.png",
+		"icon-256.png",
+		"loading-logo.png"
+	]
+}
